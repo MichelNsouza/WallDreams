c25731b860326ae430e72f0718a145451c5c87e2
Thales Souza <144599734+Thaleszx7@users.noreply.github.com>
2024-05-01 20:26:51 -0300

    TemplateEmail - Init

---
+++ src/components/TemplateEmail.vue
@@ -0,0 +1,13 @@
+<template>
+    
+    <div class="containerMaior">
+
+         
+
+
+
+    </div>
+
+
+
+</template>
--- src/views/PesquisaView.vue
+++ src/views/PesquisaView.vue
@@ -5,7 +5,7 @@ export default {
   components: {
     MeuComponente
   }
-}
+} 
 </script>
 
 <template>
