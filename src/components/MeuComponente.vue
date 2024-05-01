5dc3376d4fdbb70543d7c4dd2bee96197a48e072
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-04-30 23:04:51 -0300

    correção tag sem fechamento

--- src/components/MeuComponente.vue
+++ src/components/MeuComponente.vue
@@ -1,15 +1,4 @@
 <template>
-  <div class="meu-componente">
-    <svg
-      version="1.1"
-      baseProfile="full"
-      width="300"
-      height="200"
-      xmlns="https://www.w3.org/2000/svg">
-      <rect width="100%" height="100%" fill="black" />
-      <circle cx="150" cy="100" r="90" fill="blue" />
-    </svg>
-  </div>
 </template>
 
 <script>
