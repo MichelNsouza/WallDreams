d0de175ab945de3d398b44f77754c9d46fe4059a
Railan Ibraim <railanibraim3@gmail.com>
2024-05-04 21:46:48 -0300

    quase completo

--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -1,20 +1,45 @@
 <template>
 <div class="row d-flex justify-content-center">
 <div class="col-md-8">
-<div class="card">
-<input type="text" name="#" id="#">
-</div>
 
-<div class="texto">
-<b>Ops!!! Não encontramos nenhum Wallpaper para o termo:</b>
+<div class="input">
+    <input type="text" name="#" id="#">
 </div>
-
+    <b class="texto">Ops!!! Não encontramos nenhum Wallpaper para o termo:</b>
+</div> 
+<div class="banimento">
+    <img src="/src/assets/img/banimento 1.svg" alt="erro">
 </div>
 </div>
 </template>
+ 
 
+<style>
 
-<style scoped>
+.input{
+    height: 80px;
+    width: 1046px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30px;
+    
+}
 
+.texto{
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    font-size: 28px;
+    margin-top: 30px;
+    
+}
+.banimento{
+   display: flex;
+   justify-content: center;
+   margin-top: 50px;
+   height: 329px;
+  
+}
 
 </style>
