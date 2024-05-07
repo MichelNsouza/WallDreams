9129f88376b31aa25d6d9ca13841917e5a64660a
Thales Souza <144599734+Thaleszx7@users.noreply.github.com>
2024-05-06 23:23:43 -0300

    Modificação e retirada do Card dos Componentes - Commit 6

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/', 
       name: 'home',
       component: HomeView
     },
