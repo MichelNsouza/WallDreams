a8772c8e1f92ea9785bc08840acf6477c9448c84
Thales Souza <144599734+Thaleszx7@users.noreply.github.com>
2024-05-06 23:44:09 -0300

    Teste Commit - 9

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< HEAD
-import HomeView from '../views/HomeView.vue'
-import PesquisaView from '../views/PesquisaView.vue'
-import CardEmail from '../views/CardEmail.vue' 
-
-// import TodosView from '../views/TodosView.vue'
-// import PaisagensView from '../views/PaisagensView.vue'
-// import AbstratoView from '../views/AbstratoView.vue'
-// import AnimaisView from '../views/AnimaisView.vue'
-// import VeiculosView from '../views/VeiculosView.vue'
-// import OutrosView from '../views/OutrosView.vue'
-// import Erro404View from '../views/Erro404View.vue'
-=======
 import BuscarView from '@/views/BuscarView.vue'
 import HomeView from '@/views/HomeView.vue'
 import Notfound from '@/views/Notfound.vue'
 
->>>>>>> develop
 
 const router = createRouter({   
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,47 +13,6 @@ const router = createRouter({
       component: HomeView
     },
     {
-<<<<<<< HEAD
-      path: '/pesquisa',
-      name: 'pesquisa',
-      component: PesquisaView
-    },  
-    // {
-    //   path: '/todos',
-    //   name: 'todos',
-    //   component: TodosView
-    // },
-    // {
-    //   path: '/paisagens',
-    //   name: 'paisagens',
-    //   component: PaisagensView
-    // },
-    // {
-    //   path: '/abstrato',
-    //   name: 'abstrato',
-    //   component: AbstratoView
-    // },
-    // {
-    //   path: '/animais',
-    //   name: 'animais',
-    //   component: AnimaisView
-    // },
-    // {
-    //   path: '/veiculos',
-    //   name: 'veiculos',
-    //   component: VeiculosView
-    // },
-    // {
-    //   path: '/outros',
-    //   name: 'outros',
-    //   component: OutrosView
-    // },
-    // {
-    //   path: '/erro404',
-    //   name: 'erro404',
-    //   component: Erro404View
-    // }
-=======
       path: '/buscar',
       name: 'buscar',
       component: BuscarView
@@ -77,7 +22,6 @@ const router = createRouter({
       name: 'não-encontrado',
       component: Notfound
     }
->>>>>>> develop
   ]
 })
 
